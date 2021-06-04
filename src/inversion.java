92796c068a07b3635367cc03ad5059434e266535
SIDDHANT VATSYAYAN <siddhant.vatsyayan@gmail.com>
2021-06-05 00:07:18 +0530

    Update inversion.java

--- src/inversion.java
+++ src/inversion.java
@@ -4,7 +4,8 @@ public class inversion
 	{
      int[] arr = {9,1,7,2,8};
      int count =0;
-     for(int i=0;i<arr.length-1;i++) {
+     for(int i=0;i<arr.length-1;i++) 
+     {
     	 for(int j=i+1;j<arr.length;j++) 
     	 {
     		 if(arr[i]>arr[j]) 
