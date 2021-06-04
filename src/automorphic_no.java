1497b384c4a4118436096daa5518579fdcd31994
SIDDHANT VATSYAYAN <siddhant.vatsyayan@gmail.com>
2021-06-05 00:07:47 +0530

    Update automorphic_no.java

--- src/automorphic_no.java
+++ src/automorphic_no.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class automorphic_no
 {
-	public static void main(String args[])  
+	public static void main(String []args)  
 	{  
 	Scanner in = new Scanner(System.in);  
 	System.out.print("Enter a number to check: ");  
