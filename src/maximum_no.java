ba52cc65d95dbed11ffef89c8f04efdc8de6d5fe
SIDDHANT VATSYAYAN <siddhant.vatsyayan@gmail.com>
2021-06-06 20:47:15 +0530

    Update maximum_no.java

--- src/maximum_no.java
+++ src/maximum_no.java
@@ -4,12 +4,12 @@ public class maximum_no
 	public static void main(String []args)  
 	{  
 	int a, b, c;  
-	System.out.println("Enter three integers: ");  
+	System.out.println("Input 3 integers: ");  
 	Scanner in = new Scanner(System.in);  
 	a=in.nextInt();  
 	b=in.nextInt();  
 	c=in.nextInt();  
-	System.out.println("Maximum number is : ");
+	System.out.println("Maximum is : ");
 	System.out.println((a>b&&a>c)?a:(b>a&&b>c)?b:c);
 	
 	    }
