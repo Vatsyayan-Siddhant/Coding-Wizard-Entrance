8a30ea28d15356143dd655af1766558b6ba31c96
SIDDHANT VATSYAYAN <siddhant.vatsyayan@gmail.com>
2021-06-06 20:48:54 +0530

    Update q10.java

--- src/q10.java
+++ src/q10.java
@@ -3,9 +3,9 @@ public class q10
 {
 	   public static void main(String[] args) 
 	   {
-	        Scanner sc = new Scanner(System.in);
+	        Scanner in = new Scanner(System.in);
 	        System.out.print("Input number: ");
-	        String x=sc.nextLine();
+	        String x=in.nextLine();
 	        	System.out.println("value of "+ x + " + " + x + x + " + " + x + x + x + "  " +"is :" +"  "+(Integer.parseInt(x) + Integer.parseInt(x + x) + Integer.parseInt(x + x + x)));
 	    }
 } 
